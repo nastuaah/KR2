cae25676dd5322417f6544989117ae1378470083
nastuaah <homenko.nastya2004@yandex.ru>
2025-05-27 21:45:16 +0300

    Changing settings

--- kr2/kr2/settings.py
+++ kr2/kr2/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = "django-insecure-9d2$1=!navs!+d&$32le@nxp-n6n^e3bw7nq%qd6%y#8=7po2g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
